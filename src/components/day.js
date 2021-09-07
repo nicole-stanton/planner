72f415544acac3a5744d36c6a8477b9d1fff0f51
Nicole Stanton <nicole.stanton@wetransfer.com>
2021-09-07 15:37:33 +0100

    render dates for next 5 days and style

--- src/components/day.js
+++ src/components/day.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+export function Day() {
+    const plannerDays = []
+    const startingDay = new Date();
 
-export function Day(){
-    return(
-        <h2 className='header'>Day</h2>
+    for (var i = 0; i < 5; i++) {
+        plannerDays.push(startingDay.getDate() + i);
+    }
+
+    return (
+        <div className='days-container'>
+            {plannerDays.map(day => {
+                return (
+                    <div className='days-container__day'>
+                        <span className='days-container__date' key={i}>{day}</span>
+                    </div>
+                )
+            })}
+        </div>
+
     )
 }
 
